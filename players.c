ed57908ae811db0fca93d8729b8682533dbe7f5f
Michael M <124087552+michaelm6453@users.noreply.github.com>
2025-02-08 09:57:29 -0500

    Added update_score logic

--- players.c
+++ players.c
@@ -20,5 +20,9 @@ bool player_exists(player *players, int num_players, char *name)
 // player given their name
 void update_score(player *players, int num_players, char *name, int score)
 {
-    
+    for (int i=0; i<num_players; ++i){
+        if (strcmp(name, players[i].name) != 0)continue;
+        update_score_index(players, i, score);
+        return;
+    }
 }
